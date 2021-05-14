7f0c7f9248bbab5937ca9285fa09661302a04f4a
mahesh <maheshkumar201095@gmail.com>
2021-05-14 11:00:53 +0530

    firstcommit

---
+++ project27/ipl/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from ipl.models import *
+
+# Register your models here.
+
+admin.site.register(DelhiCapitalsTable)
+admin.site.register(MumbaiIndiansTable)
+admin.site.register(CSKTable)
+admin.site.register(PunjabKingsTable)
+admin.site.register(KKRTable)
+admin.site.register(SRHTable)
+admin.site.register(RRTable)
+admin.site.register(RCBTable)
